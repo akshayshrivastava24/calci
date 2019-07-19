f59f22e75be4a3f959d2a29827fb801fec1fd217
beingbing10 <39857730+beingbing10@users.noreply.github.com>
2019-07-18 22:01:32 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,12 +1,15 @@
 if [ "$param" == "A" ]
 then
 	 cat index.html > /var/www/html/index.html
+	 echo "A"
 elif [ "$param" == "B" ]
 then
 	mv B.html /var/www/html
+	echo "B"
 elif [ "$param" == "C" ]
 then
 	mv C.html /var/www/html
+	echo "C"
 fi
 
 
